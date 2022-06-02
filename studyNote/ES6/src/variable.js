8309b05fb7e323bfd1ebb5e68b75d3f3edd1dca6
LiujiaHuan <1395622672@qq.com>
2022-06-03 00:15:37 +0800

    update

---
+++ studyNote/ES6/src/variable.js
@@ -0,0 +1,17 @@
+//https://zh.javascript.info/variables#shi-yong-bian-liang
+let admin,
+    name1
+name1 = 'john';
+admin = name1;
+alert(admin);
+
+
+
+//https://zh.javascript.info/variables#gei-chu-zheng-que-de-ming-zi
+let ourPlantName;
+let currentUserName;
+
+
+
+//https://zh.javascript.info/variables#da-xie-de-chang-liang
+null
